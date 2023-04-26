f08b04fe1e1539805a2960ba07602d5d96bf7769
Sachin Baghel <baghel.sachin1996@gmail.com>
2023-04-26 13:14:53 +0530

    authSlice

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,10 @@
-// import { configure } from "@testing-library/react"
-// import { createStoreHook } from "react-redux"
-// import { configureStore, createStore } from "@reduxjs/toolkit"
-// import { legacy_createStore as createStore} from 'redux'
-import { legacy_createStore as createStore,legacy_createStore} from 'redux'
 import { configureStore, createSlice } from '@reduxjs/toolkit'
-import { shallowEqual } from 'react-redux'
-const initialState={counter:0,showCounter:true}
+
+const initialCounterState={counter:0,showCounter:true}
 
 const counterSlice=createSlice({
    name:'counter',
-   initialState:initialState,
+   initialState:initialCounterState,
    reducers:{
     increment(state){
 state.counter++
@@ -26,11 +21,28 @@ state.counter--
 
    }
 })
+const initialAuthState={
+    isAuthenticated:false
+}
+const authSlice=createSlice({
+    name:'authentication',
+    initialState:initialAuthState,
+    reducers:{
+        login(state){
+            state.isAuthenticated=true
+
+        },
+        logout(state){
+            state.isAuthenticated=false
+        }
+    }
+
+})
 
 const store=configureStore({
-    reducer:counterSlice.reducer
+    reducer:{counter:counterSlice.reducer,auth:authSlice.reducer}
 })
 export const counterActions=counterSlice.actions
-
+export const authActions=authSlice.actions
 
 export default store
